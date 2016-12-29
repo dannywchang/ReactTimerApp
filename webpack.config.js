bb4bf5d65e065a902b99163181ffa9f6e2125843
dannywchang <dannywchang@gmail.com>
2016-12-29 15:27:02 +0800

    add timer component & test

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
       applicationStyles: 'app/styles/app.scss',
       Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
+      TimerForm: 'app/components/TimerForm.jsx',
       Countdown: 'app/components/Countdown.jsx',
       Clock: 'app/components/Clock.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
